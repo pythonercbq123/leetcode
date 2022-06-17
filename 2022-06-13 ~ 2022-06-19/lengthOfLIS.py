df962e14c54a36222bd3bf1e43d3afe7f39ebcb2
pythonercbq123 <46085607+pythonercbq123@users.noreply.github.com>
2022-06-17 11:45:04 +0800

    add: LIS O(log n)实现

--- 2022-06-13 ~ 2022-06-19/lengthOfLIS.py
+++ 2022-06-13 ~ 2022-06-19/lengthOfLIS.py
@@ -20,3 +20,29 @@ class Solution:
         for i in range(n):
             res = max(res, dp[i])
         return res
+
+    def lengthOfLIS2(self, nums: List[int]) -> int:
+        top = [0] * len(nums)
+        piles = 0
+        for i in range(len(nums)):
+            poker = nums[i]
+            left = 0
+            right = piles - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if top[mid] > poker:
+                    right = mid - 1
+                elif top[mid] < poker:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            if left == piles:
+                piles += 1
+            top[left] = poker
+        return piles
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.lengthOfLIS2([0,1,0,3,2,3])
+    print(r)
