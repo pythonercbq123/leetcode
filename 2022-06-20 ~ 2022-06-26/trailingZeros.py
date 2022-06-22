d9d02339e7a93581866a5394acb62ff0f8f1de74
pythonercbq123 <46085607+pythonercbq123@users.noreply.github.com>
2022-06-22 10:55:53 +0800

    add: 求n!中结果中尾部0的个数

---
+++ 2022-06-20 ~ 2022-06-26/trailingZeros.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def trailingZeros(self, n):
+        ans = 0
+        for i in range(5, n + 1, 5):
+            while i % 5 == 0:
+                i //= 5
+                ans += 1
+        return ans
