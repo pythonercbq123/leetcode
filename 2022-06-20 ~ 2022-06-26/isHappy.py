a747ed4a20429aa34073dd235526ffb3c87c5692
pythonercbq123 <46085607+pythonercbq123@users.noreply.github.com>
2022-06-22 10:29:50 +0800

    add: 快慢指针解法, O(1)空间复杂度

--- 2022-06-20 ~ 2022-06-26/isHappy.py
+++ 2022-06-20 ~ 2022-06-26/isHappy.py
@@ -13,3 +13,11 @@ class Solution:
             n, mod = divmod(n, 10)
             total += mod ** 2
         return total
+
+    def slow_fast(self, n: int) -> bool:
+        slow = n
+        fast = self.get_next(n)
+        while fast != 1 and slow != fast:
+            slow = self.get_next(n)
+            fast = self.get_next(self.get_next(fast))
+        return fast == 1
