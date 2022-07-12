4cc08b737ee9ab1a7b642585c1ae2816502a7a5e
pythonercbq123 <46085607+pythonercbq123@users.noreply.github.com>
2022-07-13 00:50:43 +0800

    add: 单调队列 实现 滑动窗口

---
+++ 2022-07-11 ~ 202-07-17/maxSlidingWindow.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        window = MonotonicQueue()
+        n = len(nums)
+        for i in range(n):
+            if i < k - 1:
+                # 先填满窗口的前k-1个
+                window.push(nums[i])
+            else:
+                window.push(nums[i])
+                res.append(window.max())
+                window.pop(nums[i - k + 1])
+        return res
+
+
+class MonotonicQueue:
+    def __init__(self):
+        from collections import deque
+        self.q = deque()
+
+    def push(self, n):
+        while self.q and self.q[-1] < n:
+            self.q.pop()
+        self.q.append(n)
+
+    def max(self):
+        return max(self.q[0])
+
+    def pop(self, n):
+        if n == self.q[0]:
+            self.q.popleft()
