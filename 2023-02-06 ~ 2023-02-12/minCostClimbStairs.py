cea6d7c1e404736af8e83ecbb9e25c4bcf41ae3a
pythonercbq123 <46085607+pythonercbq123@users.noreply.github.com>
2023-02-07 08:52:18 +0800

    add:dp. 爬楼梯的最小成本。

---
+++ 2023-02-06 ~ 2023-02-12/minCostClimbStairs.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def min_cost_climb_stairs(self, cost: List[int]) -> int:
+        # dp[i]表示从i级台阶往上爬的最小成本
+        n = len(cost)
+        if n <= 2:
+            return min(cost)
+        dp = [0] * n
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2, n):
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+        return min(dp[n - 1], dp[n - 2])
+
+
+if __name__ == '__main__':
+    s = Solution()
+    ret = s.min_cost_climb_stairs([1, 100, 1, 1, 100, 1])
+    print(ret)
