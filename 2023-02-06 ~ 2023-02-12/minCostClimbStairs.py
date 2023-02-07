8d87f26682915cec66bda8bc34c8edd981032531
pythonercbq123 <46085607+pythonercbq123@users.noreply.github.com>
2023-02-07 08:57:17 +0800

    add: 进一步优化空间复杂度为O(1).

--- 2023-02-06 ~ 2023-02-12/minCostClimbStairs.py
+++ 2023-02-06 ~ 2023-02-12/minCostClimbStairs.py
@@ -7,12 +7,14 @@ class Solution:
         n = len(cost)
         if n <= 2:
             return min(cost)
-        dp = [0] * n
+        dp = [0] * 2
         dp[0] = cost[0]
         dp[1] = cost[1]
         for i in range(2, n):
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
-        return min(dp[n - 1], dp[n - 2])
+            # dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+            dp[i % 2] = min(dp[(i - 1) % 2], dp[(i - 2) % 2]) + cost[i]
+        # return min(dp[n - 1], dp[n - 2])
+        return min(dp[0], dp[1])
 
 
 if __name__ == '__main__':
