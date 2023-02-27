fdbe361ee86687beeadd0010c23c349d4c40487a
pythonercbq123 <46085607+pythonercbq123@users.noreply.github.com>
2023-02-27 19:16:05 +0800

    add: 广度优先 最大岛屿面积

--- 2023-02-20 ~ 2023-02-26/permutationSum.py
+++ 2023-02-20 ~ 2023-02-26/permutationSum.py
@@ -1,25 +1,39 @@
 from typing import List
+from collections import deque
 
 
 class Solution:
-    def permutation_sum(self, nums: List[int], target: int) -> int:
-        """
-        找出一个数组满足和为target的组合数目
-        dp[i]定义为和为i的满足条件的排列数。
-        :param nums:
-        :param target:
-        :return:
-        """
-        dp = [0] * (target + 1)
-        dp[0] = 1
-        for i in range(1, target + 1):
-            for num in nums:
-                if i >= num:
-                    dp[i] += dp[i - num]
-        return dp[target]
+    def max_area(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = [[0] * cols for _ in range(rows)]
+        max_area = 0
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1 and not visited[row][col]:
+                    area = self.get_max_area(visited, grid, row, col)
+                    max_area = max(area, max_area)
+        return max_area
+
+    def get_max_area(self, visited, grid, row, col):
+        q = deque()
+        q.append((row, col))
+        visited[row][col] = 1
+        dxy = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        area = 0
+        while q:
+            r, c = q.popleft()
+            area += 1
+            for dx, dy in dxy:
+                x = r + dx
+                y = c + dy
+                if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 1 and visited[x][y] == 0:
+                    q.append((x, y))
+                    visited[x][y] = 1
+        return area
 
 
 if __name__ == '__main__':
     s = Solution()
-    ret = s.permutation_sum([2, 1, 3], 3)
+    ret = s.max_area([[1, 1, 0, 0, 1], [1, 0, 0, 1, 0], [1, 1, 0, 1, 0], [0, 0, 1, 0, 0]])
     print(ret)
